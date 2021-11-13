1ceca30b8d24d685c902584e97b4dcea08577272
sakdeniz <38104512+sakdeniz@users.noreply.github.com>
2021-11-13 14:27:26 +0300

    Add navcoin-js and bitcore-lib version info to about page

--- app/src/pages/About.vue
+++ app/src/pages/About.vue
@@ -1,11 +1,19 @@
 <template>
 	<v-ons-page>
-  		<custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
+		<custom-toolbar v-bind="toolbarInfo"></custom-toolbar>
 		<v-ons-card>
-	        <span class="about-logo-text">NEXT</span>
-			<p>Build 1.0.6</p>
+			<span class="about-logo-text">NEXT</span>
+			<p>Build 1.0.7</p>
 			<p>Developed by Navcoin Developers</p>
 			<a href="https://www.nextwallet.org">https://www.nextwallet.org</a>
+			<p>
+				<h5>navcoin-js</h5>
+				Build 1.0.12
+			</p>
+			<p>
+				<h5>bitcore-lib</h5>
+				Build 0.15.12
+			</p>
 		</v-ons-card>
 	</v-ons-page>
 </template>
